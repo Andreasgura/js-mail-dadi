8b0649b1e5269f727e03c6c5ec813a5ccbd8954b
Andreasgura <andre-56@hotmail.it>
2024-03-11 16:17:47 +0100

    messaggio errore

--- js/script.js
+++ js/script.js
@@ -3,14 +3,21 @@ Mail
 Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
-
 */
-
-let emailInput = document.getElementById(email);
-let emailButton = document.getElementById(btnEmail);
+let emailButton = document.getElementById(btnemail);
+console.log(emailButton);
 let emailList = [];
-let emailContact;
+let trovata = false;
 for(i=0; i<20; i++){
     emailList.push(`name.surname${getRndInteger(1, 100)}@gmail.com`)
 };
 console.log(emailList)
+
+emailButton.addEventListener('click',function(){
+let emailInput = document.getElementById(email).value;
+
+for(i=0; i=emailList.length; i++)
+    if(emailInput === emailList[i] ){
+        console.log('email nel database')
+    }
+});
