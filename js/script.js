697b34fa10b6559e0dc30e5767986cbbad2bb016
Andrea Sgura <andre-56@hotmail.it>
2024-07-26 12:02:05 +0200

    refactory pt1

--- js/script.js
+++ js/script.js
@@ -15,26 +15,33 @@ btGameOne.addEventListener('click', function () {
     let emailButton = document.getElementById('btnemail');
     console.log(emailButton);
     let emailList = [];
-    let trovata = false;
     for (let i = 0; i < 20; i++) {
         emailList.push(`name.surname${getRndInteger(1, 100)}@gmail.com`)
     };
     console.log(emailList)
     let emailResult = document.getElementById('emailresult')
-    emailButton.addEventListener('click', function () {
-        let emailInput = document.getElementById('email').value;
-
-        for (let i = 0; i < emailList.length; i++)
-            if (emailInput.toLowerCase() === emailList[i].toLowerCase()) {
-                // console.log('email nel database')
-                trovata = true;
-            };
-        if (trovata) {
+     emailButton.addEventListener('click', function () {
+         let emailInput = document.getElementById('email').value;
+        
+         if (emailList.includes(emailInput.toLowerCase())){
             emailResult.innerText = `L\'email è presente nel nostro database`
         } else {
             emailResult.innerText = `Spiacenti, l\'email non è presente nel nostro database`
-        };
-    });
+        }
+        
+         //  for (let i = 0; i < emailList.length; i++)
+        //      if (emailInput.toLowerCase() === emailList[i].toLowerCase()) {
+        //          // console.log('email nel database')
+        //          trovata = true;
+        //      };
+        //  if (trovata) {
+        //      emailResult.innerText = `L\'email è presente nel nostro database`
+        //  } else {
+        //      emailResult.innerText = `Spiacenti, l\'email non è presente nel nostro database`
+        //  };
+     });
+
+    
 });
 
 btGameTwo.addEventListener('click', function (){
